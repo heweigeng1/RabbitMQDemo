ff44493d15825e27e015cf0bfd7b4b43ae80e075
heweigeng1 <6318275@qq.com>
2019-12-14 18:14:14 +0800

    添加项目文件。

---
+++ Client/C_交换机.cs
@@ -0,0 +1,124 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Client
+{
+    public class C_交换机
+    {
+        public static void 订阅模式()
+        {
+            Console.WriteLine("Start");
+            Console.WriteLine("host:");
+            var host = Console.ReadLine();
+            Console.WriteLine("port:");
+            var portstr = Console.ReadLine();
+            int.TryParse(portstr, out int port);
+            IConnectionFactory conFactory = new ConnectionFactory//创建连接工厂对象
+            {
+                HostName = host,//IP地址
+                Port = port,//端口号
+                UserName = "admin",//用户账号
+                Password = "admin"//用户密码
+            };
+            Console.WriteLine("exchange name");
+            string exchangeName = Console.ReadLine();
+            Console.WriteLine("fanout_订阅发布,direct_路由模式,topic_通配符模式");
+            string type = "direct";
+            Console.WriteLine("routingKey name:");
+            string routingKey = Console.ReadLine();
+            using (IConnection conn = conFactory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    channel.BasicQos(0, 1, false);
+                    //声明一个队列
+                    channel.ExchangeDeclare(
+                      exchange: exchangeName,//消息队列名称
+                      type: type
+                      );
+                    //创建消费者对象
+                    var consumer = new EventingBasicConsumer(channel);
+                    //创建队列
+                    string queueName = exchangeName + "_" + Guid.NewGuid().ToString("N");
+                    channel.QueueDeclare(queueName, false, false, false, null);
+                    //绑定队列与交换机
+                    channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: routingKey);
+                    consumer.Received += (model, ea) =>
+                    {
+                        byte[] message = ea.Body;//接收到的消息
+                        int.TryParse(Encoding.UTF8.GetString(message), out int num);
+                        if (num == 8)
+                        {
+                            Thread.Sleep(10000);
+                        }
+                        Console.WriteLine("接收到信息为:" + Encoding.UTF8.GetString(message));
+                        //手动确认
+                        channel.BasicAck(ea.DeliveryTag, true);
+                    };
+                    //消费者开启监听(autoAck=false) 关闭自动确认
+                    channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        public static void 路由模式()
+        {
+            Console.WriteLine("Start");
+            Console.WriteLine("host:");
+            var host = Console.ReadLine();
+            Console.WriteLine("port:");
+            var portstr = Console.ReadLine();
+            int.TryParse(portstr, out int port);
+            IConnectionFactory conFactory = new ConnectionFactory//创建连接工厂对象
+            {
+                HostName = host,//IP地址
+                Port = port,//端口号
+                UserName = "admin",//用户账号
+                Password = "admin"//用户密码
+            };
+            Console.WriteLine("exchange name");
+            string exchangeName = Console.ReadLine();
+            Console.WriteLine("exchange type");
+            string type = Console.ReadLine();
+            using (IConnection conn = conFactory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    channel.BasicQos(0, 1, false);
+                    //声明一个队列
+                    channel.ExchangeDeclare(
+                      exchange: exchangeName,//消息队列名称
+                      type: type
+                      );
+                    //创建消费者对象
+                    var consumer = new EventingBasicConsumer(channel);
+                    //创建队列
+                    string queueName = exchangeName + "_" + Guid.NewGuid().ToString("N");
+                    channel.QueueDeclare(queueName, false, false, false, null);
+                    //绑定队列与交换机
+                    channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: "");
+                    consumer.Received += (model, ea) =>
+                    {
+                        byte[] message = ea.Body;//接收到的消息
+                        int.TryParse(Encoding.UTF8.GetString(message), out int num);
+                        if (num == 8)
+                        {
+                            Thread.Sleep(10000);
+                        }
+                        Console.WriteLine("接收到信息为:" + Encoding.UTF8.GetString(message));
+                        //手动确认
+                        channel.BasicAck(ea.DeliveryTag, true);
+                    };
+                    //消费者开启监听(autoAck=false) 关闭自动确认
+                    channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,68 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+using System.Timers;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Regiest();
+            //能者多劳模式.Go();
+            C_交换机.订阅模式();
+        }
+
+        public static void Regiest()
+        {
+            Console.WriteLine("Client Start");
+            Console.WriteLine("host:");
+            var host = Console.ReadLine();
+            Console.WriteLine("port:");
+            var portstr = Console.ReadLine();
+            int.TryParse(portstr, out int port);
+            IConnectionFactory conFactory = new ConnectionFactory//创建连接工厂对象
+            {
+                HostName = host,//IP地址
+                Port = port,//端口号
+                UserName = "admin",//用户账号
+                Password = "admin"//用户密码
+            };
+            Console.WriteLine("queue name");
+            string queueName = Console.ReadLine();
+            using (IConnection conn = conFactory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    //channel.BasicQos(0, 3, false);
+                    //声明一个队列
+                    channel.QueueDeclare(
+                      queue: queueName,//消息队列名称
+                      durable: false,//是否缓存
+                      exclusive: false,
+                      autoDelete: false,
+                      arguments: null
+                       );
+                    //创建消费者对象
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        byte[] message = ea.Body;//接收到的消息
+                        int.TryParse(Encoding.UTF8.GetString(message), out int num);
+                        if (num==8)
+                        {
+                            Thread.Sleep(5000);
+                        }
+                        Console.WriteLine("接收到信息为:" + Encoding.UTF8.GetString(message));
+                    };
+                    //消费者开启监听
+                    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
